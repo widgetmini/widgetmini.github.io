cb02fc68a03f9d53815f93cc5bfe0f5593b669d0
dung.pham@silkwires-dev.com <kimthiendung@gmail.com>
2022-09-26 22:17:38 +0700

    update image effect

---
+++ modules/use-effect.js
@@ -0,0 +1,1 @@
+import{d as E,i as m,e as a}from"./css.js";let d=i=>(n,u)=>{let s=([e,f],t)=>{if(t)a(e)&&e();else return[e||n(),f]};E(([e,f]=[])=>((f||!f)&&(f&&m(f,u)?e=e||!0:(a(e)&&e(),e=null)),[e,u]),i==1&&s,i==2&&s)},p=d(2);export{p as u};
